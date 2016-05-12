6345743c43b7281230a16e6372e9cb6e3804824c
it-rockstars <rainer.haering@it-rockstars.com>
2016-05-12 12:48:49 +0200

    overview

--- src/app/overview/overview.controller.js
+++ src/app/overview/overview.controller.js
@@ -6,20 +6,26 @@
     .controller('OverviewController', OverviewController);
 
   /** @ngInject */
-  function OverviewController($timeout, feature) {
+  function OverviewController($timeout, feature, metacoin) {
     var vm = this;
     vm.amount = 200;
     vm.features = [];
+    vm.balance;
 
     activate();
 
     function activate() {
       fillFeatures();
+      fillBalance();
     }
 
 
     function fillFeatures() {
       vm.features = feature.getFeatures();
+    }
+
+    function fillBalance() {
+      vm.balance = metacoin.getBalance();
     }
 
   }
